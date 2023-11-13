ae7a21f625eea0d045846b56f29aea20b23cc2ee
pcpmark04 <ryanmark.delosreyes@pcp.org.ph>
2023-11-13 15:47:53 +0800

    NewsEvent Responsive with bug

--- pcp-website/src/components/HomeComponents/NewsEvents.jsx
+++ pcp-website/src/components/HomeComponents/NewsEvents.jsx
@@ -1,77 +1,74 @@
 import Carousel from 'react-bootstrap/Carousel';
-import { useState } from 'react';
-import {GrNext} from 'react-icons/gr';
-import {GrPrevious} from 'react-icons/gr';
+import React, { useState, useEffect } from 'react';
+import { GrNext } from 'react-icons/gr';
+import { GrPrevious } from 'react-icons/gr';
 import "../css/HomeCss/NewsEvents.css";
 
+function NewsEvents({ NewsSlides }) {
+  const [index, setIndex] = useState(0);
+  const [numVisibleItems, setNumVisibleItems] = useState(3);
+  const [sizeCarousel, setSizeCarousel] = useState(false);
 
-function NewsEvents({NewsSlides}) {
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
 
-    const [index, setIndex] = useState(0);
-    const handleSelect = (selectedIndex) => {
-        setIndex(selectedIndex);
+  // Next and Previous Icon
+  const customPrevIcon = <span className='carousel-btn prev'><GrPrevious /></span>;
+  const customNextIcon = <span className='carousel-btn next'><GrNext /></span>;
+
+  useEffect(() => {
+    const updateNumVisibleItems = () => {
+      if (window.innerWidth < 768) {
+        setNumVisibleItems(1);
+        setSizeCarousel(true);
+      } else {
+        setNumVisibleItems(3);
+        setSizeCarousel(false);
+      }
     };
 
-    // Next and Prevoius Icon 
-    const customPrevIcon = <span className='carousel-btn prev'><GrPrevious/></span>;
-    const customNextIcon = <span className='carousel-btn next'><GrNext/></span>;
+    window.addEventListener('resize', updateNumVisibleItems);
 
-    return (
-        <div className='container-content'>
-            <h1>Latest News & Events</h1>
-            <Carousel 
-                className='carousel-slides'
-                activeIndex={index}
-                onSelect={handleSelect}
-                prevIcon={customPrevIcon} // Use the custom previous icon
-                nextIcon={customNextIcon} // Use the custom next icon   
-                interval={null}
-            >
-                {NewsSlides.map((slides) => (
-                    <Carousel.Item className='carousel-item' key={slides.id} > 
-                            <div className='card-news-events'>
-                                <div className='card-items'>
-                                    <h3>{slides.title}</h3>
-                                    <p className='date'>{slides.date}</p>
-                                    <p className='text-content'>
-                                    {slides.content}
-                                    </p>
-                                    <a 
-                                        href={slides.readMore} 
-                                        className='read-more'>
-                                            Read More
-                                    </a>
-                                </div>
-                                <div className='card-items'>
-                                    <h3>{slides.title2}</h3>
-                                    <p className='date'>{slides.date2}</p>
-                                    <p className='text-content'>
-                                    {slides.content2}
-                                    </p>
-                                    <a  
-                                        href={slides.readMore2} 
-                                        className='read-more'>
-                                            Read More
-                                    </a>
-                                </div>
-                                <div className='card-items'>
-                                    <h3>{slides.title3}</h3>
-                                    <p className='date'>{slides.date3}</p>
-                                    <p className='text-content'>
-                                    {slides.content3}
-                                    </p>
-                                    <a  
-                                        href={slides.readMore3} 
-                                        className='read-more'>
-                                            Read More
-                                    </a>
-                                </div>
-                            </div>
-                    </Carousel.Item>
-                ))}
-            </Carousel>
-        </div>
-    );
+    // Call it initially to set the initial number of visible items
+    updateNumVisibleItems();
+
+    return () => {
+      window.removeEventListener('resize', updateNumVisibleItems);
+    };
+  }, []);
+
+  return (
+    <div className='container-content'>
+      <h1>Latest News & Events</h1>
+      <Carousel
+        className={sizeCarousel ? 'mobile-news-events-slide' : 'carousel-slides'}
+        activeIndex={index}
+        onSelect={handleSelect}
+        prevIcon={customPrevIcon}
+        nextIcon={customNextIcon}
+        interval={null}
+        controls={window.innerWidth >= 768}
+      >
+        {NewsSlides.map((slides) => (
+          <Carousel.Item className='carousel-item' key={slides.id}>
+            <div className='card-news-events'>
+              {[...Array(numVisibleItems)].map((_, i) => (
+                <div className='card-items' key={i}>
+                  <h3>{slides[`title${i + 1}`]}</h3>
+                  <p className='date'>{slides[`date${i + 1}`]}</p>
+                  <p className='text-content'>{slides[`content${i + 1}`]}</p>
+                  <a href={slides[`readMore${i + 1}`]} className='read-more'>
+                    Read More
+                  </a>
+                </div>
+              ))}
+            </div>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
+  );
 }
 
 export default NewsEvents;
