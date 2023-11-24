ebe4468102c23a2402ad8b83c3eaf6a433e5b8a0
pcpmark04 <ryanmark.delosreyes@pcp.org.ph>
2023-11-24 16:02:40 +0800

    PAHMOC MOA bugs in downloading pdf file

---
+++ pcp-website/src/components/HMOComponents/HMOModals/MOA.modal.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import Modal from 'react-bootstrap/Modal';
+import { GiOpenFolder } from 'react-icons/gi'
+import '../../css/HMOCss/MOA.modal.css'
+import PAHMOC from '../../img/HMOImage/PAHMOC/16_PAHMOC_Acknowledgement_of_PCP_Batch_16_2023_06_14_2023.pdf'
+
+
+export default function MOAModal({pahmocData, ...props}) {
+  return (
+    <Modal
+        {...props}
+        size="xl"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+    >
+        <Modal.Header 
+            closeButton 
+            className='MOA-header'
+        >
+            PAHMOC
+        </Modal.Header>
+        <Modal.Body className='MOA-modal-body'>
+        {pahmocData.map((pahmocFile) => (
+            <div className='moa-dl-forms-modal'>
+                <h3><GiOpenFolder/> {pahmocFile.title}</h3>
+                <div className='moa-dl-btn'>
+                    <a 
+                        href={pahmocFile.viewFile} 
+                        className='moa-btn from-top'
+                    >
+                        View
+                    </a>
+                    <a 
+                        href={PAHMOC} 
+                        download // Provide a suggested filename
+                        className='moa-btn from-right'
+                    >
+                        Download
+                    </a>
+                </div>        
+            </div>
+        ))}
+        </Modal.Body>
+        <Modal.Footer>
+        <button 
+            onClick={props.onHide}
+            className='moa-modal-btn'
+        >
+            Close
+        </button>
+        </Modal.Footer>
+    </Modal>
+  )
+}
--- pcp-website/src/components/HMOComponents/MOA.jsx
+++ pcp-website/src/components/HMOComponents/MOA.jsx
@@ -1,27 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PAHMOC from '../img/HMOImage/PAHMOC.png'
 import { FaFolderOpen } from 'react-icons/fa6'
 import AHMOPI from '../img/HMOImage/AHMOPI.png'
 import '../css/HMOCss/MOA.css'
+import MOAModal from './HMOModals/MOA.modal'
+import PAHMOCData  from '../../data/HMOData/PAHMOC'
 
 export default function MOA() {
+    const [showModalPahmoc, setShowModalPahmoc ] = useState(false);
   return (
     <div className='hmo-container'>
         <div className='hmo-wrapper'>
             <div className='hmo-content-wrapper'>
                 <div className='hmo-content-header'>
-                    <img src={PAHMOC} alt='PAHMOC Image'/>
+                    <img src={PAHMOC} alt='PAHMOC'/>
                     <div className='hmo-yellow-bg'></div>
                     <div className='hmo-grey-bg'></div>
                 </div>
                 <div className='MOA-btn'>
-                    <button><FaFolderOpen/> MOA 2021-2023</button>
+                    <button
+                        onClick={() => setShowModalPahmoc(true)}
+                    ><FaFolderOpen/> MOA 2021-2023</button>
+                    <MOAModal 
+                        show={showModalPahmoc}
+                        onHide={() => setShowModalPahmoc(false)}
+                        pahmocData={PAHMOCData}
+                    />
                     <button><FaFolderOpen/> MOA 2018-2020</button>
                 </div>
             </div>
             <div className='hmo-content-wrapper'>
                 <div className='hmo-content-header'>
-                    <img src={AHMOPI} alt='PAHMOC Image'/>
+                    <img src={AHMOPI} alt='AHMOPI'/>
                     <div className='hmo-lightGreen-bg'></div>
                     <div className='hmo-grey-bg'></div>
                 </div>
